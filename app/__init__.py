7bd48195715584e0e002794e1682156e0ccb0434
AliBesher <ali.besher12@gmail.com>
2025-03-05 20:03:24 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,11 @@ def create_app():
     app = Flask(__name__)
     
     # Register blueprints
-    from app.routes.user_routes import user_routes
-    from app.routes.product_routes import product_routes
-    from app.routes.cart_routes import cart_routes
-    from app.routes.order_routes import order_routes
-    from app.routes.checkout_routes import checkout_routes
+    from app.routes import user_routes
+    from app.routes import product_routes
+    from app.routes import cart_routes
+    from app.routes import order_routes
+    from app.routes import checkout_routes
     
     app.register_blueprint(user_routes, url_prefix='/api')
     app.register_blueprint(product_routes, url_prefix='/api')
