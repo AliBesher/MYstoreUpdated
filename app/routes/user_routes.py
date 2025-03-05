a0c3a026442ae03387bbf3230dc0415737ea9055
AliBesher <ali.besher12@gmail.com>
2025-03-05 20:08:30 +0200

    Update user_routes.py

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from app.services.user_service import UserService
+from app.services import UserService
 
 user_routes = Blueprint('user_routes', __name__)
 
