3cf5befa754e79004f132587aaff9f86b02a4caa
AliBesher <ali.besher12@gmail.com>
2025-03-05 19:54:20 +0200

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,1 +1,6 @@
-
+# app/routes/__init__.py
+from app.routes.user_routes import user_routes
+from app.routes.product_routes import product_routes
+from app.routes.cart_routes import cart_routes
+from app.routes.order_routes import order_routes
+from app.routes.checkout_routes import checkout_routes
