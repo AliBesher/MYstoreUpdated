a0c0f5f61b45a2b701610dea7f1f4bba32c0aa9e
AliBesher <ali.besher12@gmail.com>
2025-03-10 19:11:10 +0200

    Update execute_query.py

--- app/db/execute_query.py
+++ app/db/execute_query.py
@@ -2,12 +2,6 @@ import pyodbc
 from app.db.connection import get_connection
 
 def execute_query(query, params=None, fetch=False):
-    """
-    Function to execute SQL queries on the database.
-    - query: Query text.
-    - params: Parameters to pass to the query (if any).
-    - fetch: Set to True if the operation needs to fetch data.
-    """
     try:
         with get_connection() as connection:
             with connection.cursor() as cursor:
@@ -17,9 +11,11 @@ def execute_query(query, params=None, fetch=False):
                     cursor.execute(query)
 
                 if fetch:
-                    return cursor.fetchall()
+                    # تحويل الصفوف إلى قواميس
+                    columns = [column[0] for column in cursor.description]
+                    return [dict(zip(columns, row)) for row in cursor.fetchall()]
 
                 connection.commit()
     except pyodbc.Error as e:
-        print(f"Error executing query: {e}")
+        print(f"خطأ أثناء تنفيذ الاستعلام: {e}")
         return None
